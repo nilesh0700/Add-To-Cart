779c4d7a7a4cec7377f78462e7de38c39ee358a1
nilesh0700 <nileshthengde0700@gmail.com>
2023-11-02 19:29:42 +0530

    added: design,onValue functon, updated: additem()

--- index.js
+++ index.js
@@ -1,8 +1,66 @@
-const inputFieldEl = document.getElementById("input-field");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-5ac74-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
-addButtonEl.addEventListener("click", function(){
-    let inputValue = inputFieldEl.value;
-    console.log(inputValue);
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
 
+    clearInputFieldEl();
+
+    // addItemToShoppingListEl(inputValue);
+    
+    
+
+    
 })
+// the onValye() function is triggered when there is any addition, updation and deletion in a realtime dataset
+onValue(shoppingListInDB, function(snapshot){
+    let snapValues = snapshot.val();
+    let itemsArray = Object.entries(snapValues);// get both entries therefore used Object.entries() function
+
+    clearShoppingListEl(); // clear the shopping list to avoid the duplication of the entries
+    
+    for(let i = 0; i < itemsArray.length; i++){
+
+        let currentItem = itemsArray[i];
+        let currentItemID = currentItem[0];
+        let currentItemValue = currentItem[1];
+
+        addItemToShoppingListEl(currentItem); // adding both id and value from database
+    }
+})
+
+function clearInputFieldEl(){
+    inputFieldEl.value = ""; 
+}
+
+function addItemToShoppingListEl(item){
+    // shoppingListEl.innerHTML += `<li>${item}</li>`
+
+    let itemID = item[0];
+    let itemValue = item[1];
+    let newEl = document.createElement("li");
+
+
+    newEl.textContent = itemValue;
+
+     shoppingListEl.append(newEl);
+}
+
+function clearShoppingListEl(){
+    shoppingListEl.innerHTML = "";
+}
+
